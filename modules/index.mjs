8c51d37ef36e93d4bf7587cb289e4b2c7ffc4de9
André <andreglock@gmail.com>
2021-06-22 13:32:22 +0200

    done

---
+++ modules/index.mjs
@@ -0,0 +1,7 @@
+import { clickImages } from "./click.mjs";
+import { hoverHeader } from "./hover.mjs";
+import { scrollWindow } from "./scroll.mjs";
+
+clickImages();
+hoverHeader();
+scrollWindow();
